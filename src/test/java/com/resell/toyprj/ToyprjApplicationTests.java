48bec232f01d457b83cbbd406594810b8d2062af
shkimm5189 <devsaintkim@gmail.com>
2022-03-27 20:59:10 +0900

    첫 소스 코드

---
+++ src/test/java/com/resell/toyprj/ToyprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resell.toyprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
