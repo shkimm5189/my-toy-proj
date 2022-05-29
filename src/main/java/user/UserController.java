725cf65605391cd0f72cf1f4e95cc3907a1a3957
shkimm5189 <shkimm5189@gmail.com>
2022-05-29 20:44:38 +0900

    Add : template 추가
    - URL 링크 필요
    
    - boostrap 제대로 동작하는지 확인 / 연동 필요
    - js script 처리 필요

--- src/main/java/user/UserController.java
+++ src/main/java/user/UserController.java
@@ -1,7 +1,10 @@
 package user;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class UserController {
+
 }
